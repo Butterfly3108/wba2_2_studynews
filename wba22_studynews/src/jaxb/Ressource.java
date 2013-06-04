330c321ae997da6a03576d973d6195bc88a21192
Butterfly <daniela.kucharczyk@gmx.de>
2013-06-04 11:59:04 +0200

    xmpp client angefangen

--- wba22_studynews/src/jaxb/Ressource.java
+++ wba22_studynews/src/jaxb/Ressource.java
@@ -20,50 +20,50 @@ import javax.xml.bind.Unmarshaller;
 public abstract class  Ressource {
 
 
-@SuppressWarnings("rawtypes")
-public Object unmarshal(Class clazz, String xml) throws JAXBException, IOException {
-	
-	Object object = new Object();
-	JAXBContext jc = JAXBContext.newInstance(clazz);
-	Unmarshaller unmarshaller = jc.createUnmarshaller();
-	InputStream inputStream = new FileInputStream(xml);
-	Reader reader = new InputStreamReader(inputStream,"UTF-8");
-	try {
-	object = (Object) unmarshaller.unmarshal(reader);
-	} finally {
-	reader.close();
+	@SuppressWarnings("rawtypes")
+	public Object unmarshal(Class clazz, String xml) throws JAXBException, IOException {
+		
+		Object object = new Object();
+		JAXBContext jc = JAXBContext.newInstance(clazz);
+		Unmarshaller unmarshaller = jc.createUnmarshaller();
+		InputStream inputStream = new FileInputStream(xml);
+		Reader reader = new InputStreamReader(inputStream,"UTF-8");
+		try {
+			object = (Object) unmarshaller.unmarshal(reader);
+		} finally {
+			reader.close();
+		}
+		return object;
+		
 	}
-	return object;
 	
-}
-
-@SuppressWarnings("rawtypes")
-public void marshal(Class clazz, Object object, String xml, String schemaLoc) throws JAXBException {
-JAXBContext jc = JAXBContext.newInstance(clazz);
-Marshaller marshaller = jc.createMarshaller();
-marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-marshaller.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");
-marshaller.setProperty("jaxb.schemaLocation", schemaLoc);
-marshaller.marshal(object, new File(xml));
-}
-
-@GET
-@Produces(MediaType.APPLICATION_XML)
-public abstract Object getAll() throws JAXBException, IOException;
-
-@POST
-public Response post() {
-return null;
-}
-
-@PUT
-public Response put() {
-return null;
-}
-
-@DELETE
-public Response delete() {
-return null;
-}
+	@SuppressWarnings("rawtypes")
+	public void marshal(Class clazz, Object object, String xml, String schemaLoc) throws JAXBException {
+		JAXBContext jc = JAXBContext.newInstance(clazz);
+		Marshaller marshaller = jc.createMarshaller();
+		marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+		marshaller.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");
+		marshaller.setProperty("jaxb.schemaLocation", schemaLoc);
+		marshaller.marshal(object, new File(xml));
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_XML)
+	public abstract Object getAll() throws JAXBException, IOException;
+	
+	@POST
+	public Response post() {
+		return null;
+	}
+	
+	@PUT
+	public Response put() {
+		return null;
+	}
+	
+	@DELETE
+	public Response delete() {
+		return null;
+	}
 
 }
